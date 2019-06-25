2fa6d1b893ef279b486e1cf61355b8cdedaafb45
Daniele Di Salvo <disi91@hotmail.it>
2019-06-25 15:31:03 +0200

    INSERT search handle functions

--- js/search.js
+++ js/search.js
@@ -1,31 +1,89 @@
+flattenData = data => {
+    const r = []
+
+    if (data == null) {
+        return r;
+    }
+
+    for (p of data) {
+        if (p.subsections == null) {
+            continue
+        }
+
+        for (lg of p.subsections) {
+            r.push(...lg.subsections)
+        }
+    }
+
+    return r
+}
+
+const searchResults = (query, data) => {
+    const res = []
+
+    for (d of data) {
+        console.log("d", d)
+        if (d.title.toLowerCase().indexOf(query.toLowerCase())) {
+            res.push(d)
+        }
+    }
+
+    return res
+}
+
+const toggleSearchBar = () => {
+    document.querySelector(".searchbox").classList.toggle("open")
+}
+
+function debouce(fn, wait, immediate) {
+    let timeout;
+    return () => {
+        const later = () => {
+            timeout = null;
+            if (!immediate) {
+                fn(...arguments)
+            }
+        }
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+        if (callNow) {
+            fn(...arguments)
+        }
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function(){
     console.log("Hello DOM!");
+
+    const data = flattenData(window.data.subsections);
+    console.log(data)
+
+    document.querySelector("li.search").addEventListener("click", toggleSearchBar)
 
     const search = document.querySelector("#search-input")
     const results = document.querySelector("#results")
 
+    //const searchfn = debouce(searchResults, 300, false)
+    const searchfn = searchResults
+
+    searchResults(data, "")
+
     search.addEventListener("input", () => {
         //console.log(search.value)
             
-        const res = [];
-
-        for (d of data) {
-            const text = search.value.toLowerCase()
-            if (d.title.indexOf(text) != -1) {
-                res.push(d)
-            }
-        }
-
-        console.log(res)
+        const res = searchfn(search.value, data)
 
         results.innerHTML = "";
+
         for (r of res) {
-            const i = document.createElement("div");
-            i.setAttribute("data-url", r.page + '#' + r.anchor)
+            const i = document.createElement("a");
+            i.setAttribute("href", "wcag.html#" + r.metadata.source)
+            i.setAttribute("title", r.title)
             i.innerText = r.title;
 
             i.addEventListener('click', e => {
-                location.href = e.target.getAttribute("data-url")
+                toggleSearchBar()
             })
 
             results.appendChild(i)
