482da536b63abe5bf963640dfa8422cbf84942ed
Omar Polo <omar.polo@europecom.net>
2019-06-25 20:21:58 +0200

    fixed search and debounce fn

--- js/search.js
+++ js/search.js
@@ -1,8 +1,12 @@
+/**
+ * Takes a json representation of a `Section' and flatten it into a list
+ * of sections.
+ */
 flattenData = data => {
     const r = []
 
     if (data == null) {
-        return r;
+        return r
     }
 
     for (p of data) {
@@ -18,75 +22,73 @@ flattenData = data => {
     return r
 }
 
-const searchResults = (query, data) => {
+/**
+ * Takes a functions and a interval and return a function. The returned
+ * function is guaranteed to call the given callback *at most* every
+ * `time' milliseconds.
+ */
+const debounce = (fn, time) => {
+    let timeout;
+
+    return function() {
+      const functionCall = () => fn.apply(this, arguments);
+    
+      clearTimeout(timeout);
+      timeout = setTimeout(functionCall, time);
+    }
+}
+
+/**
+ * generate and renders the list of results.
+ *
+ * @param query   string    the user query
+ * @param data    section[] the list of sections to search
+ * @param results node      the dom node to render the risult in
+ */
+const searchResults = (query, data, results) => {
     const res = []
+    
+    results.innerHTML = ''
 
     for (d of data) {
-        console.log("d", d)
-        if (d.title.toLowerCase().indexOf(query.toLowerCase())) {
-            res.push(d)
+        if (d.title.toLowerCase().indexOf(query.toLowerCase()) != -1) {
+            if (d.metadata.source == null)
+                console.log("Metadata for", d.title, "is missing")
+            
+            const i = document.createElement("a")
+            i.setAttribute("href", "wcag.html#" + d.metadata.source)
+            i.setAttribute("title", d.title)
+            i.innerText = d.title
+            
+            i.addEventListener('click', toggleSearchBar)
+            results.appendChild(i)
         }
     }
 
     return res
 }
 
+/**
+ * Toggle the search bar
+ */
 const toggleSearchBar = () => {
     document.querySelector(".searchbox").classList.toggle("open")
 }
 
-function debouce(fn, wait, immediate) {
-    let timeout;
-    return () => {
-        const later = () => {
-            timeout = null;
-            if (!immediate) {
-                fn(...arguments)
-            }
-        }
-        const callNow = immediate && !timeout;
-        clearTimeout(timeout)
-        timeout = setTimeout(later, wait)
-        if (callNow) {
-            fn(...arguments)
-        }
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("Hello DOM!");
+    const data = flattenData(window.data.subsections)
 
-    const data = flattenData(window.data.subsections);
-    console.log(data)
-
-    document.querySelector("li.search").addEventListener("click", toggleSearchBar)
+    document
+        .querySelector("li.search")
+        .addEventListener("click", toggleSearchBar)
 
     const search = document.querySelector("#search-input")
     const results = document.querySelector("#results")
+    const searchfn = debounce(searchResults, 300)
 
-    //const searchfn = debouce(searchResults, 300, false)
-    const searchfn = searchResults
-
-    searchResults(data, "")
+    searchfn("", data, results)
 
     search.addEventListener("input", () => {
-        //console.log(search.value)
-            
-        const res = searchfn(search.value, data)
-
-        results.innerHTML = "";
-
-        for (r of res) {
-            const i = document.createElement("a");
-            i.setAttribute("href", "wcag.html#" + r.metadata.source)
-            i.setAttribute("title", r.title)
-            i.innerText = r.title;
-
-            i.addEventListener('click', e => {
-                toggleSearchBar()
-            })
-
-            results.appendChild(i)
-        }
-    });
+        searchfn(search.value, data, results)
+    })
 });
